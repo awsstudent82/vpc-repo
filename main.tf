a5291c21d4a29bfe9a06d5407c99d83c2bd5c501
awsstudent82 <awsstudent82@gmail.com>
2024-05-08 11:43:37 +0100

    vpc name updated

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "aws_vpc" "main4" {
+resource "aws_vpc" "main2" {
   cidr_block       = "192.168.0.0/16"
   instance_tenancy = "default"
   tags = {
-    Name = "vpc4"
+    Name = "vpc3"
   }
 }
